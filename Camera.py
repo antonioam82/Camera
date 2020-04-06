b7d53bf70d0a7fd3b48022103cc5e38232634eef
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-04-06 20:44:52 +0200

    Update Camera.py

--- Camera.py
+++ Camera.py
@@ -8,7 +8,7 @@ def visor():
     global photo
     ret, frame = vid.read()
     real_color = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    photo=ImageTk.PhotoImage(image=Image.fromarray(real_color))
+    photo = ImageTk.PhotoImage(image=Image.fromarray(real_color))
     canvas.create_image(0,0,image=photo,anchor=NW)#0,0
     ventana.after(15,visor)
     
@@ -17,18 +17,18 @@ ventana.title("Camri")
 ventana.resizable(0,0)
 ventana['bg']='black'
 
-vid=cv2.VideoCapture(0)
+vid = cv2.VideoCapture(0)
 ima_w = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
 ima_h = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
 #ELEMENTOS.
-label = Label(ventana,text="CAM",font=15,bg='blue',fg='white')
+label = Label(ventana,text="CAM",font = 15,bg ='blue',fg ='white')
 label.pack(side=TOP,fill=BOTH)
-canvas=Canvas(ventana,bg='red',width=ima_w,height=ima_h)
+canvas = Canvas(ventana,bg ='red',width = ima_w,height = ima_h)
 canvas.pack()
-btnScreenshot=Button(ventana,text="Screenshot",width=30,bg='goldenrod2',
+btnScreenshot = Button(ventana,text = "Screenshot",width = 30,bg ='goldenrod2',
                     activebackground='red')
-btnScreenshot.pack(anchor=CENTER,expand=True)
+btnScreenshot.pack(anchor = CENTER,expand = True)
 visor()
 ventana.mainloop()
 
