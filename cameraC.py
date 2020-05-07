b534e1614be2d6f32f3e1f2dda795dc980609e80
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-05-07 15:20:42 +0200

    Update cameraC.py

--- cameraC.py
+++ cameraC.py
@@ -9,139 +9,43 @@ from PIL import Image, ImageTk
 import threading
 import time
 import os
-import glob
+#import glob
 
-if not os.path.exists(os.getcwd()+"\CAMARA_VIDEOS"):
-    os.makedirs("CAMARA_VIDEOS")
-os.chdir(os.getcwd()+"\CAMARA_VIDEOS")
+class CameraApp():
 
-def future_file():
-    name = "output.avi"
-    count = 0
-    for i in glob.glob('*.avi'):
-        if "output" in i:
-            count+=1
-    if count>0:
-        name = "output"+"("+str(count)+")"+".avi"
-    return name
+    def __init__(self):
 
-class App:
-    def __init__(self,font_video=0):
-        global name_file
-        self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        self.out = cv2.VideoWriter(name_file,self.fourcc, 20.0, (640,480))
-        name_file = future_file()
-        self.hours = 0
-        self.minuts = 0
-        self.seconds = 0
+        self.fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+        self.name_file = "output.avi"
+        self.out = cv2.VideoWriter(self.name_file,self.fourcc, 20.0, (640,480))
+        #name_file = future_file()
         
-        self.appName = "camera"
-        self.ventana = Tk()
-        self.ventana.title(self.appName)
-        self.ventana['bg']='black'
-        self.font_video=font_video
-        self.recording=False
-        self.vid=VideoCaptura(self.font_video)#!!!!!!!!!!!!!!!!!!!!!!!!!
-        self.label=Label(self.ventana,text=self.appName,font=15,bg='blue',
-                         fg='white').pack(side=TOP,fill=BOTH)
-        
-        self.canvas=Canvas(self.ventana,bg='red',width=self.vid.width,height=self.vid.height)
-        self.canvas.pack()
-        self.btnScreenshot = Button(self.ventana,text="Screenshot",width=28,bg='goldenrod2',
-                    activebackground='red')
-        self.btnRecord = Button(self.ventana,text='Record',width=29,bg='red',
-                                fg='white',command=self.record)
-        self.btnRecord.pack(side=LEFT)        
-        self.btnScreenshot.pack(side=RIGHT)
-        self.counter = Label(self.ventana,text='00:00:00',bg='black',fg='red',width=27,height=2,font=('Arial',11))
-        self.counter.pack(side=LEFT)        
-
-
-        self.visor()
-        self.ventana.mainloop()
-        
-    def captura(self):
-        ver,frame=self.vid.get_frame()
-        if ver:
-            image="IMG-"+time.strftime("%H-%M-%S-%d-%m")+".jpg"
-            cv2.imwrite(image,cv2.cvtColor(frame,cv2.COLOR_BGR2RGB))
-            
-    def visor(self):
-        ret, frame=self.vid.get_frame()
-        if ret:
-            self.photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
-            self.canvas.create_image(0,0,image=self.photo,anchor=NW)#0,0
-            if self.recording == True:
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                self.out.write(frame)
-            self.ventana.after(15,self.visor)
-
-    def record(self):
-        global name_file
-        if self.recording == False:
-            self.recording = True
-            self.clear_timer()
-            self.init_timer()
-            self.btnRecord.configure(text='Stop')
-            
-        else:
-            self.recording = False
-            self.btnRecord.configure(text='Record')
-            self.counter.after_cancel(self.process)
-            name_file = future_file()
-            self.out = cv2.VideoWriter(name_file,self.fourcc, 20.0, (640,480))
-
-    def formato(self,c):
-        if c<10:
-            c="0"+str(c)
-        return c
-
-    def clear_timer(self):
         self.hours=0
         self.minuts=0
         self.seconds=0
-        
-    def cuenta(self):
-        self.counter['text'] = str(self.formato(self.hours))+":"+str(self.formato(self.minuts))+":"+str(self.formato(self.seconds))
-        self.seconds+=1
-        if self.seconds==60:
-            self.seconds=0
-            self.minuts+=1
-        if self.minuts==60:
-            self.minuts=0
-            self.hours+=1
-        self.process=self.counter.after(100,self.cuenta)
+        self.appName = "Video Recorder"
+        self.root = Tk()
+        self.root.title(self.appName)
+        self.root['bg']='black'
+        #self.font_video = self.font_video
 
-    def init_timer(self):
-        t = threading.Thread(target=self.cuenta)
-        t.start()
+        self.label=Label(self.root,text=self.appName,font=15,bg='blue',
+                         fg='white').pack(side=TOP,fill=BOTH)
         
-class VideoCaptura:
-    def __init__(self,font_video=0):
-        self.vid = cv2.VideoCapture(font_video)
-        if not self.vid.isOpened():
-            raise ValueError("No se puede usar esta camara")
-        self.width=self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)
-        self.height=self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        
-    def get_frame(self):
-        if self.vid.isOpened():
-            verif,frame=self.vid.read()
-            if verif:
-                return(verif,cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-            else:
-                return(verif,None)
-        else:
-            return(verif,None)
-        
+        self.canvas=Canvas(self.root,bg='black')
+        self.canvas.pack()
+        self.btnScreenshot = Button(self.root,text="Screenshot",width=28,bg='green',fg='white')
+        self.btnRecord = Button(self.root,text='Record',width=29,bg='green',fg='white')
+        self.btnRecord.pack(side=LEFT)        
+        self.btnScreenshot.pack(side=RIGHT)
+        self.counter = Label(self.root,text='00:00:00',bg='black',fg='red',width=27,height=2,font=('Arial',11))
+        self.counter.pack(side=LEFT)
 
-    def __del__(self):
-        if self.vid.isOpened():
-            self.vid.release()
-            os.remove(name_file)
-        print("OK")
+        self.root.mainloop()
+
 
 if __name__=="__main__":
-    App()
+    CameraApp()
+
 
 
